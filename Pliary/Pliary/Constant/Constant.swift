64837a2b7b382a541beb26054d6d97a140ecbc87
jeewoong <jeewoong@users.noreply.oss.navercorp.com>
2019-08-01 01:54:05 +0900

    Add Extension

---
+++ Pliary/Pliary/Constant/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  Pliary
+//
+//  Created by jeewoong.han on 01/08/2019.
+//  Copyright © 2019 groot.nexters.pliary. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ Pliary/Pliary/Home/Cell/HomeCardCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCardCollectionViewCell.swift
+//  Pliary
+//
+//  Created by jeewoong.han on 01/08/2019.
+//  Copyright © 2019 groot.nexters.pliary. All rights reserved.
+//
+
+import UIKit
+
+class HomeCardCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
