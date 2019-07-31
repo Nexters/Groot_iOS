830ae38ddc7f86e648ced9f822218397f5efdbf1
jeewoong <jeewoong@users.noreply.oss.navercorp.com>
2019-08-01 02:49:34 +0900

    Add tableview cell reuse identifier

---
+++ Pliary/Pliary/Detail/Cell/DiarySmallCardTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  DiarySmallCardTableViewCell.swift
+//  Pliary
+//
+//  Created by jeewoong.han on 01/08/2019.
+//  Copyright © 2019 groot.nexters.pliary. All rights reserved.
+//
+
+import UIKit
+
+class DiarySmallCardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
