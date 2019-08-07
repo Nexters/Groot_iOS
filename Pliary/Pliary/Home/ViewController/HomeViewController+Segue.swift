3b9e43c0f233cbc95f54fe0a0d75d6d608664d30
jeewoong <jeewoong@users.noreply.oss.navercorp.com>
2019-08-07 21:10:53 +0900

    Change font for home view controller

--- Pliary/Pliary/Home/ViewController/HomeViewController+Segue.swift
+++ Pliary/Pliary/Home/ViewController/HomeViewController+Segue.swift
@@ -17,6 +17,10 @@ extension HomeViewController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+        guard let selectedCell = collectionView.cellForItem(at: indexPath) as? HomeCardCollectionViewCell else {
+            return
+        }
+        
         let englishNameID = "englishName"
         let koreanNameID = "koreanName"
         let customNameID = "customName"
@@ -38,46 +42,45 @@ extension HomeViewController: UICollectionViewDelegate {
         enableHero(view: customNameLabel, id: customNameID)
         enableHero(view: nameSplitLabel, id: splitName)
         
-        if let cell = collectionView.cellForItem(at: indexPath) as? HomeCardCollectionViewCell {
-            cell.hero.isEnabled = true
-            cell.contentView.hero.modifiers = [.cascade]
-            
-            enableHero(view: cell.plantView, id: plantID)
-            enableHero(view: cell.addWaterButton, id: addWaterID)
-            enableHero(view: cell.blackWaterImageView, id: blackWaterID)
-            enableHero(view: cell.dayLeftLabel, id: dayLeftID)
-            enableHero(view: cell.contentView, id: cellID)
-        } else {
+        selectedCell.hero.isEnabled = true
+        selectedCell.contentView.hero.modifiers = [.cascade]
+        
+        enableHero(view: selectedCell.plantView, id: plantID)
+        enableHero(view: selectedCell.addWaterButton, id: addWaterID)
+        enableHero(view: selectedCell.blackWaterImageView, id: blackWaterID)
+        enableHero(view: selectedCell.dayLeftLabel, id: dayLeftID)
+        enableHero(view: selectedCell.contentView, id: cellID)
+        
+        let storyboard = UIStoryboard.init(name: StoryboardName.detail, bundle: Bundle(for: DetailViewController.self))
+        guard let detailVC = storyboard.instantiateViewController(withIdentifier: DetailViewController.identifier) as? DetailViewController else {
             return
         }
         
-        let storyboard = UIStoryboard.init(name: StoryboardName.detail, bundle: Bundle(for: DetailViewController.self))
-        if let detailVC = storyboard.instantiateViewController(withIdentifier: DetailViewController.identifier) as? DetailViewController {
-            
-            detailVC.hero.isEnabled = true
-            detailVC.hero.modalAnimationType = .none
-            detailVC.view.hero.modifiers = [.cascade]
-            
-            enableHero(view: detailVC.view, id: cellID)
-            
-            let indexPath = IndexPath.init(row: 0, section: 0)
-            detailVC.setUpTableView()
-            detailVC.tableView.reloadData()
-            detailVC.tableView.hero.isEnabled = true
-            
-            if let cell = detailVC.tableView.cellForRow(at: indexPath) as? MainDetailTableViewCell {
-                
-                enableHero(view: cell.plantView, id: plantID)
-                enableHero(view: cell.addWaterButton, id: addWaterID)
-                enableHero(view: cell.blackWaterImageView, id: blackWaterID)
-                enableHero(view: cell.dayLeftLabel, id: dayLeftID)
-                enableHero(view: cell.englishNameLabel, id: englishNameID)
-                enableHero(view: cell.koreanNameLabel, id: koreanNameID)
-                enableHero(view: cell.customNameLabel, id: customNameID)
-                enableHero(view: cell.nameSplitLabel, id: splitName)
-            }
-            
-            present(detailVC, animated: true, completion: nil)
+        detailVC.hero.isEnabled = true
+        detailVC.hero.modalAnimationType = .none
+        detailVC.view.hero.modifiers = [.cascade]
+        
+        enableHero(view: detailVC.view, id: cellID)
+        
+        
+        let index = IndexPath.init(row: 0, section: 0)
+        detailVC.setUpTableView()
+        detailVC.tableView.reloadData()
+        detailVC.tableView.hero.isEnabled = true
+        
+        guard let detailCell = detailVC.tableView.cellForRow(at: index) as? MainDetailTableViewCell else {
+            return
         }
+        
+        enableHero(view: detailCell.plantView, id: plantID)
+        enableHero(view: detailCell.addWaterButton, id: addWaterID)
+        enableHero(view: detailCell.blackWaterImageView, id: blackWaterID)
+        enableHero(view: detailCell.dayLeftLabel, id: dayLeftID)
+        enableHero(view: detailCell.englishNameLabel, id: englishNameID)
+        enableHero(view: detailCell.koreanNameLabel, id: koreanNameID)
+        enableHero(view: detailCell.customNameLabel, id: customNameID)
+        enableHero(view: detailCell.nameSplitLabel, id: splitName)
+        
+        present(detailVC, animated: true, completion: nil)
     }
 }
