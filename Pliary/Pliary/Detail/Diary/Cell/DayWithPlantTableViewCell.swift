e1a1d089c81aeb39a5adfc5d300c94f599906690
jeewoong <jeewoong@users.noreply.oss.navercorp.com>
2019-08-08 00:50:36 +0900

    Add day with plant cell

--- Pliary/Pliary/Detail/Diary/Cell/DayWithPlantTableViewCell.swift
+++ Pliary/Pliary/Detail/Diary/Cell/DayWithPlantTableViewCell.swift
@@ -1,14 +1,14 @@
 //
-//  DiarySmallCardTableViewCell.swift
+//  DayWithPlantTableViewCell.swift
 //  Pliary
 //
-//  Created by jeewoong.han on 01/08/2019.
+//  Created by jeewoong.han on 08/08/2019.
 //  Copyright © 2019 groot.nexters.pliary. All rights reserved.
 //
 
 import UIKit
 
-class DiarySmallCardTableViewCell: UITableViewCell {
+class DayWithPlantTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
---
+++ Pliary/Pliary/Detail/Diary/Cell/DiaryCardWithAllTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  DiaryCardWithAllTableViewCell.swift
+//  Pliary
+//
+//  Created by jeewoong.han on 01/08/2019.
+//  Copyright © 2019 groot.nexters.pliary. All rights reserved.
+//
+
+import UIKit
+
+class DiaryCardWithAllTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
