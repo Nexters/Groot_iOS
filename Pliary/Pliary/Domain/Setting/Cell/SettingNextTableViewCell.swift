98e8a9932e02d9a788f3b97aaa2e0de32fe41775
jeewoong <jeewoong@users.noreply.oss.navercorp.com>
2019-08-10 12:18:25 +0900

    Add cell for setting

---
+++ Pliary/Pliary/Domain/Setting/Cell/SettingNextTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SettingNextTableViewCell.swift
+//  Pliary
+//
+//  Created by jeewoong.han on 10/08/2019.
+//  Copyright © 2019 groot.nexters.pliary. All rights reserved.
+//
+
+import UIKit
+
+class SettingNextTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        selectionStyle = .none
+    }
+    
+}
---
+++ Pliary/Pliary/Domain/Setting/Cell/UserProfileView.swift
@@ -0,0 +1,24 @@
+//
+//  UserProfileView.swift
+//  Pliary
+//
+//  Created by jeewoong.han on 10/08/2019.
+//  Copyright © 2019 groot.nexters.pliary. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileView: UIView {
+    
+    static func instance() -> UserProfileView {
+        let view: UserProfileView = UIView.createViewFromNib(nibName: "UserProfileView")
+        return view
+    }
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBAction func tabCameraButton(_ sender: Any) {
+        
+    }
+    
+}
