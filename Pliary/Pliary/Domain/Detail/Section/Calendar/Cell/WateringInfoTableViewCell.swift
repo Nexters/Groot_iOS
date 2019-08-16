a5f0230f67335ea9bf6a88883ace6f3a53cc8d61
jueun <wndms0217@naver.com>
2019-08-17 08:56:20 +0900

    Update WateringInfoTableViewCell.swift

--- Pliary/Pliary/Domain/Detail/Section/Calendar/Cell/WateringInfoTableViewCell.swift
+++ Pliary/Pliary/Domain/Detail/Section/Calendar/Cell/WateringInfoTableViewCell.swift
@@ -11,16 +11,41 @@ import UIKit
 class WateringInfoTableViewCell: UITableViewCell {
 
     static let height: CGFloat = 66.5
-    let date : Int = 0
-    weak var delegate: DetailEventDelegate?
-    
+    var timestamp : Int = 0
     @IBOutlet weak var waterImage: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var msgLabel: UILabel!
-    
+   
     override func awakeFromNib() {
         super.awakeFromNib()
         selectionStyle = .none
+        
+        let date = formatterForCell.date(from:dateLabel.text ?? "")
+        self.timestamp = Int(date?.timeIntervalSince1970 ?? 0)
     }
+    
+    fileprivate let formatterForCell: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy.MM.dd"
+        return formatter
+    }()
+    @IBAction func moreButtonClicked(_ sender: Any) {
 
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let deleteAction = UIAlertAction(title: "삭제", style: .destructive, handler: { _ in
+            print("\(self.timestamp) 삭제")
+            self.removeFromSuperview()
+        })
+        alert.addAction(deleteAction)
+
+        alert.view.tintColor = .black
+        UIApplication.shared.keyWindow?.rootViewController?.present(alert, animated: true, completion: nil)
+
+        var topVC = UIApplication.shared.keyWindow?.rootViewController
+        while((topVC!.presentedViewController) != nil){
+            topVC = topVC!.presentedViewController
+        }
+        topVC?.present(alert, animated: true, completion: nil)
+        
+    }
 }
