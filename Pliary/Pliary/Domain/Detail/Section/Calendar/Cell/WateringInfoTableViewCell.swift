9803d74e5e9bdcab6a0dc4a6772481e803041ba8
jueun <wndms0217@naver.com>
2019-08-17 00:54:01 +0900

    watering info 수정

--- Pliary/Pliary/Domain/Detail/Section/Calendar/Cell/WateringInfoTableViewCell.swift
+++ Pliary/Pliary/Domain/Detail/Section/Calendar/Cell/WateringInfoTableViewCell.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class WateringInfoTableViewCell: UITableViewCell {
 
-    static let height: CGFloat = 96
+    static let height: CGFloat = 66.5
+    let date : Int = 0
+    weak var delegate: DetailEventDelegate?
     
     @IBOutlet weak var waterImage: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
@@ -20,5 +22,5 @@ class WateringInfoTableViewCell: UITableViewCell {
         super.awakeFromNib()
         selectionStyle = .none
     }
-    
+
 }
