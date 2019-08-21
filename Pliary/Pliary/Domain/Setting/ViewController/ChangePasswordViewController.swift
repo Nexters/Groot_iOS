9a711c0dc74aa02d1360a942cdf3cf0ba1be2b42
jueun <wndms0217@naver.com>
2019-08-22 00:49:11 +0900

    Update ChangePasswordViewController.swift

--- Pliary/Pliary/Domain/Setting/ViewController/ChangePasswordViewController.swift
+++ Pliary/Pliary/Domain/Setting/ViewController/ChangePasswordViewController.swift
@@ -58,6 +58,7 @@ class ChangePasswordViewController: UIViewController, UITextFieldDelegate {
                         self.completePopupLoad()
                         return
                     }
+                    return
                 }
                 // Error!
                 let manager :FirebaseAuthenticationManager = FirebaseAuthenticationManager.shared
