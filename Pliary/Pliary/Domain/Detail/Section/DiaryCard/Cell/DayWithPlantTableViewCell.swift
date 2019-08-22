9b247dbf444dfdd68bcae5dafe812511634245a9
jeewoong <jeewoong@users.noreply.oss.navercorp.com>
2019-08-23 03:08:43 +0900

    Calculate day with plant

--- Pliary/Pliary/Domain/Detail/Section/DiaryCard/Cell/DayWithPlantTableViewCell.swift
+++ Pliary/Pliary/Domain/Detail/Section/DiaryCard/Cell/DayWithPlantTableViewCell.swift
@@ -19,10 +19,30 @@ class DayWithPlantTableViewCell: UITableViewCell {
         layer.applySketchShadow( color: #colorLiteral(red: 0.3490196078, green: 0.3529411765, blue: 0.4235294118, alpha: 0.08), alpha: 0.8, x: 0, y: 9, blur: 15, spread: 0)
     }
     
+    func daysBetween(start: Date, end: Date) -> Int? {
+        let calendar = Calendar.current
+        
+        // Replace the hour (time) of both dates with 00:00
+        let date1 = calendar.startOfDay(for: start)
+        let date2 = calendar.startOfDay(for: end)
+        
+        let a = calendar.dateComponents([.day], from: date1, to: date2)
+        return a.value(for: .day)
+    }
+    
     func setUp() {
-        let day: Int = 1000
-        dayWithLabel.text = day.description + "일"
-        dayWithPlantLabel.text = (Global.shared.selectedPlant?.nickName ?? "") + "와(과) " + day.description + "일을 함게 했어요!"
+        guard let plant = Global.shared.selectedPlant else {
+            return
+        }
+        
+        let today = Date()
+        let firstDay = Date(timeIntervalSince1970: plant.firstDate)
+        
+        if let day = daysBetween(start: firstDay, end: today) {
+            dayWithLabel.text = (day + 1).description + "일"
+            dayWithPlantLabel.text = (Global.shared.selectedPlant?.nickName ?? "") + "와(과) " + (day + 1).description + "일을 함게 했어요!"
+        }
+        
     }
     
 }
